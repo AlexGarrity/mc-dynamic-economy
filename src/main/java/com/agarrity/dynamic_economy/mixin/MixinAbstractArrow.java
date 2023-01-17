52fd9321e25270a72f8441ea41a7d06c830702bc
AlexGarrity <garrity2799@gmail.com>
2023-01-17 00:36:14 +0000

    feat: Make picking up arrows add them to the economy
    Added a mixin to arrows that means that when they're picked up by a non-creative mode player, they'll be added to the economy

---
+++ src/main/java/com/agarrity/dynamic_economy/mixin/MixinAbstractArrow.java
@@ -0,0 +1,32 @@
+package com.agarrity.dynamic_economy.mixin;
+
+import com.agarrity.dynamic_economy.common.economy.resources.WorldResourceTracker;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(AbstractArrow.class)
+public abstract class MixinAbstractArrow {
+
+    @Shadow public AbstractArrow.Pickup pickup;
+
+    @Shadow protected abstract ItemStack getPickupItem();
+
+    @Inject(at=@At(value="TAIL"), method="tryPickup", locals = LocalCapture.CAPTURE_FAILHARD)
+    public void onTryPickup(Player pPlayer, CallbackInfoReturnable<Boolean> cir) {
+        if (pPlayer.getLevel().isClientSide) {
+            return;
+        }
+
+        if (this.pickup == AbstractArrow.Pickup.ALLOWED && cir.getReturnValue()) {
+            WorldResourceTracker.addItemsToEconomy(this.getPickupItem(), 1);
+        }
+    }
+
+}
